5bbb587cb317808bc4578f8db44043cbeea78aa3
Anastasia Okuonghae <aokuonghae@gmail.com>
2019-12-11 11:16:38 -0600

    exercise complete + bonus

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -9,7 +9,7 @@ export class FavPhotosComponent implements OnInit {
   photosTitle = 'Images';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
   image2 = 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/WPVG_icon_2016.svg/1024px-WPVG_icon_2016.svg.png';
-  image3 = 'https://upload.wikimedia.org/wikipedia/commons/6/6a/Godot_icon.svg';
+  image3 = 'https://media1.giphy.com/media/13HgwGsXF0aiGY/giphy.gif';
 
   constructor() { }
 
